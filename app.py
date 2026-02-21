f1a9202f148a5b1d4836f64a6fcda6fb6957fc88
Kuldeep Mishra <coolldeep@gmail.com>
2026-02-21 17:27:36 +0000

    Add FastAPI app and Dockerfile

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+def analyze_log(file_path):
+    error_count = 0
+    warning_count = 0
+    info_count = 0
+
+    if not os.path.exists(file_path):
+        return {"error": "File not found"}
+
+    with open(file_path, "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                error_count += 1
+            elif "WARNING" in line:
+                warning_count += 1
+            elif "INFO" in line:
+                info_count += 1
+
+    return {
+        "ERROR": error_count,
+        "WARNING": warning_count,
+        "INFO": info_count
+    }
+
+@app.get("/analyze")
+def analyze(file: str):
+    return analyze_log(file)
